13877949c6f34816c489fd68569eb60c5faceb74
Başar Yüksel <basar@stadion.io>
2024-05-29 15:57:26 +0200

    working fixxbuzz

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Input } from './components/Input';
 
 export const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Input />
     </View>
   );
 };
@@ -14,5 +15,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
   },
 });
+
+/*
+Fizzbuzz
+
+display an input field (that's validated)
+onchange handler for validation and running the function
+function to determine fixx or buzz
+local state to keep it fizz or buzz
+display the fizz or buzz
+
+component that displays the input field
+compnent that displasy the result
+parent coponent with state of fizz or buzz, error state (o number)
+useEffect to decide fi orr buzz (and set state)
+
+*/
---
+++ src/components/Input.tsx
@@ -0,0 +1,62 @@
+import { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+interface Props {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export const Input: React.FC = () => {
+  const [value, setValue] = useState('');
+  const [displayValue, setDisplayValue] = useState('');
+
+  useEffect(() => {
+    if (value) {
+      setDisplayValue(getFizzBuzz(value));
+    }
+  }, [value]);
+
+  const onChange = useCallback(
+    (value: string) => {
+      setValue(value);
+    },
+    [value],
+  );
+
+  const getFizzBuzz = (value: string): string => {
+    if (!value || isNaN(Number(value))) {
+      return value;
+    }
+
+    const numberValue = Number(value);
+
+    if (numberValue % 3 === 0 && numberValue % 5 === 0) {
+      return 'FizzBuzz';
+    }
+    if (numberValue % 3 === 0) {
+      return 'Fizz';
+    }
+
+    if (numberValue % 5 === 0) {
+      return 'Buzz';
+    }
+    return value;
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        value={value}
+        onChangeText={onChange}
+        style={{ width: 100, height: 40, backgroundColor: 'whitesmoke' }}
+      />
+      <Text>{displayValue}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+  },
+});
