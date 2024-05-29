26cfd52eee2bce34e5b0195f3a734b3aa353c18b
Başar Yüksel <basar@stadion.io>
2024-05-29 14:49:30 +0200

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Text, View } from 'react-native';
-
+import { StyleSheet, Text, View } from 'react-native';
 
 export const App = () => {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <Text>Hello</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
