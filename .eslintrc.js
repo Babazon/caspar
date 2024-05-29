afad44f9b73daab52cb6b1433a71a4e7120ec388
Başar Yüksel <basar@stadion.io>
2024-05-29 14:42:59 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: false,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  plugins: ['prettier', 'react-hooks', 'simple-import-sort', '@typescript-eslint', 'react-native'],
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-unused-styles': 'error',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+
+export const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
